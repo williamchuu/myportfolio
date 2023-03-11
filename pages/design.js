490a772ed020ce89d66580151a8a8214b041e7f6
William <wchu41@my.bcit.ca>
2023-03-10 18:28:09 -0800

    design projects start

---
+++ pages/design.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Navbar2 from '../components/navbar2'
+import { useState } from 'react';
+import H1 from '../components/h1';
+import Contact from '../components/contact';
+import AllDesignProjects from '../components/allDesignProjects'
+
+const inter = Inter({ subsets: ['latin'] })
+export default function Projects() {
+
+    return (
+        <>
+            <Head>
+                <title>William Chus</title>
+                <meta name="description" content="Design Projects" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="faviconLight.svg" media="(prefers-color-scheme: dark)" />
+                <link rel="icon" href="faviconDark.svg" media="(prefers-color-scheme: light)" />
+            </Head>
+            <main className={styles.main}>
+                <div>
+                    <Navbar2 />
+                    <AllDesignProjects />
+                    <Contact />
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ pages/design/BoyWithNoFace.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+import Navbar2 from '../../components/navbar2'
+import BackToTop from '../../components/backToTop'
+
+export default function BoyWithNoFace() {
+    return (
+        <>
+            <Head>
+                <title>William Chu</title>
+                <meta name="description" content="Boy With No Face" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="faviconLight.svg" media="(prefers-color-scheme: dark)" />
+                <link rel="icon" href="faviconDark.svg" media="(prefers-color-scheme: light)" />
+            </Head>
+            <main className={styles.main}>
+                <Navbar2 />
+                Hey! You're not supposed to see this yet. Come back later!
+                <BackToTop />
+            </main>
+        </>
+    )
+}

2ef506b367ccd7c91d026490315ec381e06e4c3d
Philip <philip03ho@gmail.com>
2025-07-11 14:42:23 -0700

    chore: change loading animation to 1.5x speed

--- src/app/components/LoadingAnimation/index.tsx
+++ src/app/components/LoadingAnimation/index.tsx
@@ -1,27 +1,50 @@
-"use client"
+"use client";
 import Image from "next/image";
-import Lottie from 'lottie-react';
-import animation from "../../../../public/landing/logoAni.json"
-import { useState } from "react";
-import { motion } from "framer-motion"
+import Lottie from "lottie-react";
+import animation from "../../../../public/landing/logoAni.json";
+import { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+
 export default function LoadingAnimation({
-    onComplete,
-    onClick
+  onComplete,
+  onClick,
 }: {
-    onComplete?: () => void,
-    onClick?: () => void,
+  onComplete?: () => void;
+  onClick?: () => void;
 }) {
-    return <motion.section className={`h-[100dvh] z-[5000] w-full fixed grid place-content-center text-center bg-bg `} onClick={onClick} initial={{ opacity: 1 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-        <div className="">
-            <Lottie
-                className={"w-full pt-16 md:pt-32 "}
-                animationData={animation}
-                loop={false}
-                onComplete={onComplete}
-            />
-        </div>
-        <motion.p initial={{ opacity: 0, y: 15 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .5, duration: 1, ease: "easeInOut" }} exit={{}}
-            className="text-accent pt-16 md:pt-32 cursor-pointer">(Click anywhere to skip)
-            </motion.p>
+  const lottieRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(1.5); 
+    }
+  }, []);
+
+  return (
+    <motion.section
+      className={`h-[100dvh] z-[5000] w-full fixed grid place-content-center text-center bg-bg`}
+      onClick={onClick}
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="">
+        <Lottie
+          className={"w-full pt-16 md:pt-32"}
+          animationData={animation}
+          loop={false}
+          onComplete={onComplete}
+          lottieRef={lottieRef}
+        />
+      </div>
+      <motion.p
+        initial={{ opacity: 0, y: 15 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5, duration: 1, ease: "easeInOut" }}
+        className="text-accent pt-16 md:pt-32 cursor-pointer"
+      >
+        (Click anywhere to skip)
+      </motion.p>
     </motion.section>
+  );
 }
